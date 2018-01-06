34dae7f0d743daabc5e66598f881fa92e33f5469
hayatochiri <dev@sucret.sakura.ne.jp>
2018-01-07 03:53:06 +0900

    holicerBot: DBv1アップデート処理を追加
    DBに必要なテーブルの一括作成処理

--- holicerBot/db-initialize.go
+++ holicerBot/db-initialize.go
@@ -75,10 +75,100 @@ func updateDB(db *sql.DB) error {
 		}
 
 		switch db_version {
+		case 0:
+			updateDBv1(db)
 		default:
 			already_updated = true
 		}
 
+	}
+
+	return nil
+}
+
+func updateDBv1(db *sql.DB) error {
+	var err error
+
+	tx, err := db.Begin()
+	if err != nil {
+		return err
+	}
+
+	query := `
+		update master set db_version = 1 where id = 1;
+
+		create table taverns (
+			id         integer primary key autoincrement,
+			name_jp    text    null,
+			name_en    text    null,
+			is_removed integer not null
+		);
+
+		create table groups (
+			id           integer primary key autoincrement,
+			uuid         text    not null    unique,
+			name_jp      text    null,
+			name_en      text    null,
+			started_time text    null,
+			tavern_id    integer not null,
+			total_price  integer null,
+			tax_rate     integer null,
+			cleard_time  text    null
+		);
+
+		create table users (
+			id         integer primary key autoincrement,
+			twitter_id text    null        unique,
+			email      text    null        unique,
+			name       text    not null,
+			avator     blob    null,
+			status     text    not null,
+			group_id   integer null
+		);
+
+		create table menus (
+			id         integer primary key autoincrement,
+			tavern_id  integer not null,
+			name_jp    text    null,
+			name_en    text    null,
+			price      integer not null,
+			tax_rate   integer null,
+			is_removed integer not null
+		);
+
+		create table users_log (
+			id        integer primary key autoincrement,
+			user_id   integer not null,
+			group_id  integer not null,
+			status    text    not null,
+			timestamp text    not null
+		);
+
+		create table leave_log (
+			id          integer primary key autoincrement,
+			user_log_id integer not null    unique,
+			pay         integer null
+		);
+
+		create table orders_log (
+			id        integer primary key autoincrement,
+			user_id   integer not null,
+			group_id  integer not null,
+			menu_id   integer not null,
+			split     integer not null,
+			status    text    not null,
+			timestamp text    not null
+		);
+	`
+
+	_, err = db.Exec(query)
+	if err != nil {
+		return err
+	}
+
+	err = tx.Commit()
+	if err != nil {
+		return err
 	}
 
 	return nil
