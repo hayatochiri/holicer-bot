2d0f7a8c368fe408d93f3aa6d71438f24b5e046b
hayatochiri <dev@sucret.sakura.ne.jp>
2018-01-08 04:24:30 +0900

    holicerBot_test: メモリ上にSQLite3データベースを作成、戻り値として返す関数を追加
    自動テストの際はいちいちファイル読み書きが発生すると動作が遅くなるためメモリのみにデータベースを作成しテストを行う。

--- holicerBot/db-initialize_test.go
+++ holicerBot/db-initialize_test.go
@@ -6,3 +6,13 @@ import (
 
 	_ "github.com/mattn/go-sqlite3"
 )
+
+func openDBonMemory(t *testing.T) *sql.DB {
+	db, err := sql.Open("sqlite3", ":memory:")
+
+	if err != nil {
+		t.Fatalf("Error occurred when sql.Open() (%v)", err)
+	}
+
+	return db
+}
