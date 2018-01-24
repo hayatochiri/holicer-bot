d5ea7d65b14b56271408e666b7da035571259c61
hayatochiri <dev@sucret.sakura.ne.jp>
2018-01-24 22:13:28 +0900

    holicerBot_test: Tavern構造体に、構造体同時の一致比較するメソッドを定義

--- holicerBot/tavern_test.go
+++ holicerBot/tavern_test.go
@@ -4,3 +4,28 @@ import (
 	"fmt"
 	"testing"
 )
+
+func (actual Tavern) compare(t *testing.T, expect Tavern) {
+	output := "Compare\n"
+	output += "\tID\n"
+	output += fmt.Sprintf("\t\tactual : %d\n", actual.Id)
+	output += fmt.Sprintf("\t\texpect : %d\n", expect.Id)
+	output += "\tNameJA\n"
+	output += fmt.Sprintf("\t\tactual : \"%s\"\n", actual.NameJA)
+	output += fmt.Sprintf("\t\texpect : \"%s\"\n", expect.NameJA)
+	output += "\tNameEN\n"
+	output += fmt.Sprintf("\t\tactual : \"%s\"\n", actual.NameEN)
+	output += fmt.Sprintf("\t\texpect : \"%s\"\n", expect.NameEN)
+	output += "\tIsRemoved\n"
+	output += "\t\tactual : " + TO_BOOL[actual.IsRemoved] + "\n"
+	output += "\t\texpect : " + TO_BOOL[expect.IsRemoved] + "\n"
+
+	if actual.Id == expect.Id &&
+		actual.NameJA == expect.NameJA &&
+		actual.NameEN == expect.NameEN &&
+		actual.IsRemoved == expect.IsRemoved {
+		t.Logf(output)
+	} else {
+		t.Errorf(output)
+	}
+}
