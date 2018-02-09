3a7a3cc2e1a604b5e62c903e7695decf0896dc47
hayatochiri <dev@sucret.sakura.ne.jp>
2018-02-10 03:16:45 +0900

    holicerBot_test: 居酒屋レコードを削除するテストコードを実装
    居酒屋レコードを10件登録し、うち数件を削除。その処理が適切に行えているかをチェック。

--- holicerBot/tavern_test.go
+++ holicerBot/tavern_test.go
@@ -96,3 +96,70 @@ func TestAddTavern(t *testing.T) {
 		actual.compare(t, expect)
 	}
 }
+
+func TestRemoveTavern(t *testing.T) {
+	openDBonMemory(t)
+	defer db.Close()
+
+	if err := initializeDB(); err != nil {
+		t.Fatalf("Error occurred when initializeDB() (%v)", err)
+	}
+
+	remove_list := []Tavern{
+		{NameJA: `居酒屋(No.1)`, NameEN: `Tavern(No.1)`, Description: `詳細01`, Locate: `場所001`, Homepage: `https://www.test.tavern1.local`, IsRemoved: FALSE},
+		{NameJA: `居酒屋(No.2)`, NameEN: `Tavern(No.2)`, Description: `詳細02`, Locate: `場所002`, Homepage: `https://www.test.tavern2.local`, IsRemoved: TRUE},
+		{NameJA: `居酒屋(No.3)`, NameEN: `Tavern(No.3)`, IsRemoved: TRUE},
+		{NameJA: `居酒屋(No.4)`, NameEN: `Tavern(No.4)`, Description: `詳細04`, Locate: `場所004`, Homepage: `https://www.test.tavern4.local`, IsRemoved: TRUE},
+		{NameJA: `居酒屋(No.5)`, NameEN: `Tavern(No.5)`, Description: `詳細05`, Locate: `場所005`, Homepage: `https://www.test.tavern5.local`, IsRemoved: FALSE},
+		{NameJA: `居酒屋(No.6)`, NameEN: `Tavern(No.6)`, Description: `詳細06`, Locate: `場所006`, Homepage: `https://www.test.tavern6.local`, IsRemoved: TRUE},
+		{NameJA: `居酒屋(No.7)`, NameEN: `Tavern(No.7)`, Description: `詳細07`, Locate: `場所007`, Homepage: `https://www.test.tavern7.local`, IsRemoved: FALSE},
+		{NameJA: `居酒屋(No.8)`, NameEN: `Tavern(No.8)`, Description: `詳細08`, Locate: `場所008`, Homepage: `https://www.test.tavern8.local`, IsRemoved: FALSE},
+		{NameJA: `居酒屋(No.9)`, NameEN: `Tavern(No.9)`, Description: `詳細09`, Locate: `場所009`, Homepage: `https://www.test.tavern9.local`, IsRemoved: FALSE},
+		{NameJA: `居酒屋(No.10)`, NameEN: `Tavern(No.10)`, Description: `詳細10`, Locate: `場所010`, Homepage: `https://www.test.tavern10.local`, IsRemoved: TRUE},
+	}
+
+	for _, item := range remove_list {
+		inserted_id, err := AddTavern(AddTavernParams{
+			NameJA:      item.NameJA,
+			NameEN:      item.NameEN,
+			Description: item.Description,
+			Locate:      item.Locate,
+			Homepage:    item.Homepage,
+		})
+		if err != nil {
+			t.Fatalf("Error occurred when AddTavern() (%v)", err)
+		}
+
+		if item.IsRemoved == FALSE {
+			continue
+		}
+
+		t.Logf("Remove tavern(%v)", inserted_id)
+		is_removed, err := RemoveTavern(inserted_id)
+		if err != nil {
+			t.Fatalf("Error occurred when RemoveTavern() (%v)", err)
+		}
+		if !is_removed {
+			t.Fatalf("Could not remove the record that could be removed")
+		}
+
+		is_removed, err = RemoveTavern(inserted_id)
+		if err != nil {
+			t.Fatalf("Error occurred when RemoveTavern() (%v)", err)
+		}
+		if is_removed {
+			t.Fatalf("Removed the removed record")
+		}
+	}
+
+	var actual Tavern
+	for index, expect := range remove_list {
+		expect.Id = int64(index) + 1
+		query := `select * from taverns where id = ?`
+		row := db.QueryRow(query, expect.Id)
+
+		row.Scan(&actual.Id, &actual.NameJA, &actual.NameEN, &actual.Description, &actual.Locate, &actual.Homepage, &actual.IsRemoved)
+		actual.compare(t, expect)
+	}
+
+}
